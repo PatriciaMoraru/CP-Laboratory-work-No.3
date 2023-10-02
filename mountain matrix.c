036d1018cd63a570fbbd59959fcccf1b99091b58
PatriciaMoraru <patriciamoraruv@gmail.com>
2023-10-02 20:13:02 +0300

    Updated version

--- mountain matrix.c
+++ mountain matrix.c
@@ -1,9 +1,62 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void generator_matrix(int arr[][100], int max, int n, int m)
+{
+	int i, j;
+	
+	for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+        	arr[i][j] = (rand() % max) + 1;
+		}
+    }    
+    return;
+}
+
+void convert_2D_to_1D(int arr2[][100], int n, int m, int b[])
+{
+    int x = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            b[x] = arr2[i][j];
+            x++;
+        }
+    }
+}
+
+void bubble_sort(int x[], int n, int m)
+{
+    int j, aux;
+    int k = n * m;
+
+    for (int i = 0; i < k-1; i++)
+    {
+        for (j = 0; j < k-i-1; j++)
+        {
+            if (x[j] > x[j+1])
+            {
+                aux = x[j+1];
+                x[j+1] = x[j];
+                x[j] = aux;
+            }
+        }
+    }
+}
 
 int main ()
 {
     int n, m, i, j, k;
     int a[100][100];
+    int a1[1000];
+    int a2[100][100];
+    
+    srand(time(NULL));
 
     printf("Input the size of the matrix NxM:\n");
 
@@ -13,6 +66,20 @@ int main ()
     printf("columns (m) = ");
     scanf("%d", &m);
     printf("\n\n\n");
+    
+    generator_matrix(a, 1000, n, m);
+    printf("The generated unsorted matrix: \n");
+	
+	for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+            printf("%5d", a[i][j]);
+            
+        printf("\n");
+    }
+    
+    convert_2D_to_1D(a, n, m, a1);
+    bubble_sort(a1, n, m);
 
     int up = 0;
     int down = 0;
@@ -21,11 +88,11 @@ int main ()
 
     i = 0;
     j = 0;
-    k = 1;
+    k = 0;  
 
-    while (k <= n * m)
+    while (k < n * m)
     {
-        a[i][j] = k;
+        a2[i][j] = a1[k];
 
         if (i == up && j < m - right - 1)
             ++j; 
@@ -48,11 +115,14 @@ int main ()
         }
         ++k;
     }
-
+	
+	printf("\n The Sorted Matrix in a Spiral: \n");
+	
+	
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < m; j++)
-            printf("%5d", a[i][j]);
+            printf("%5d", a2[i][j]);
             
         printf("\n");
     }
